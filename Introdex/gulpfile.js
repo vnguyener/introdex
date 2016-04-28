1c092e274b0073d8d9b95b611516708809751236
vnguyener <its.vnguyen@gmail.com>
2016-04-28 14:06:21 -0500

    base structure
    added gulp file, src and build paths feels ---sew gewd

---
+++ Introdex/gulpfile.js
@@ -0,0 +1,42 @@
+var gulp = require("gulp");
+var del = require("del");
+var tsc = require("gulp-typescript");
+var tsProject = tsc.createProject("tsconfig.json");
+var tslint = require("gulp-tslint");
+
+
+// cleaning
+gulp.task("clean", function() {
+   return del(["build"]) ;
+});
+
+// compiling typescript
+gulp.task("compile", function() {
+   var tsResult = gulp.src("src/**/*.ts")
+        .pipe(tsc(tsProject));
+   return tsResult.js
+        .pipe(gulp.dest("build"));
+});
+
+gulp.task("resources", function() {
+   return gulp.src(["src/**/*", "!**/*.ts"])
+        .pipe(gulp.dest("build"));
+});
+
+gulp.task("libs", function() {
+    return gulp.src([
+            "es6-shim/es6-shim.min.js",
+            "systemjs/dist/system-polyfills.js",
+            "angular2/bundles/angular2-polyfills.js",
+            "angular2/es6/dev/src/testing/shims_for_IE.js",
+            "systemjs/dist/system.src.js",
+            "rxjs/bundles/Rx.js",
+            "angular2/bundles/angular2.dev.js",
+            "angular2/bundles/router.dev.js"
+        ], {cwd: "node_modules/**"})
+        .pipe(gulp.dest("build/lib"));
+});
+
+gulp.task("build", ["compile", "resources", "libs"], function() {
+    console.log("Building the project ...");
+});
---
+++ README.md
@@ -0,0 +1,8 @@
+**Global Deps**
+
+```
+gulp and gulp-cli (3.9.1)
+typings (0.7.+)
+typescript (1.8.+)
+ts-node (0.5.5)
+```
